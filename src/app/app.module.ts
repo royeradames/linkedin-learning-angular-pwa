cc20ed6707112aa06220bf42e466882562582f7d
Royer Adames <royeraadames@gmail.com>
2023-02-20 22:39:57 -0500

    preparing the angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NgModule } from '@angular/core';
+import { GeolocationService } from "./geolocation.service";
 import { AppComponent } from './app.component';
+import { DataService } from "./data.service";
+
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MdButtonModule, MdIconModule, MdInputModule, MdSelectModule, MdSliderModule,
+         MdToolbarModule, MdCardModule, MdSlideToggleModule } from "@angular/material";
+import 'hammerjs';
+import { ListComponent } from './list/list.component';
+import { CoffeeComponent } from './coffee/coffee.component';
+import { Routes, RouterModule } from "@angular/router";
+
+import { FormsModule } from "@angular/forms";
+
+const routes : Routes = [
+  { path: '', component: ListComponent },
+  { path: 'coffee', component: CoffeeComponent },
+  { path: 'coffee/:id', component: CoffeeComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    CoffeeComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes),
+    FormsModule,
+    BrowserModule, BrowserAnimationsModule,
+    MdButtonModule, MdIconModule, MdInputModule, MdSelectModule, MdSliderModule,
+    MdToolbarModule, MdCardModule, MdSlideToggleModule
   ],
-  providers: [],
+  providers: [GeolocationService, DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/logic/Coffee.ts
@@ -0,0 +1,19 @@
+import { TastingRating } from "./TastingRating";
+import { PlaceLocation } from "./PlaceLocation";
+
+export class Coffee {
+
+    // Properties
+    type: string;
+    rating: number;
+    notes: string;
+    tastingRating: TastingRating;
+
+    constructor(public name: string = "", 
+                public place: string = "", 
+                public location: PlaceLocation = null) {
+
+        this.location = new PlaceLocation();
+    }
+
+}
